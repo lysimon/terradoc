85dac6167202e5124586bb02e3ff767fa2c8b779
Laurent Simon <laurent.simon@gmx.com>
2018-12-16 18:54:41 +0100

    Adding more tests

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -1,6 +1,6 @@
 import unittest
 from lib import configuration
-
+import os
 
 class TestStorage(unittest.TestCase):
 
@@ -17,8 +17,21 @@ class TestStorage(unittest.TestCase):
         with self.assertRaises(ValueError):
             configuration.Configuration("file")
 
+    def test_configuration__correct_environment_no_path__raise_value_error(self):
+        os.environ["TERRADOC_NAME"] = "toto"
+        with self.assertRaises(ValueError):
+            configuration.Configuration("environment")
 
-    def test_get_configuration_from_file__wrong_file__raise_value_error(self):
+    def test_configuration__correct_environment_no_path__raise_value_error(self):
+        os.environ["TERRADOC_NAME"] = "toto"
+        os.environ["TERRADOC_PATH"] = "/data/myfiles"
+        conf = configuration.Configuration("environment")
+        self.assertEquals(1, len(conf.locations))
+        self.assertEquals("toto", conf.locations[0].name)
+        self.assertEquals("/data/myfiles", conf.locations[0].path)
+        self.assertEquals(True, conf.locations[0].recursive)
+
+    def test_set_configuration_from_file__wrong_file__raise_value_error(self):
         with self.assertRaises(ValueError):
             configuration.Configuration("")
         with self.assertRaises(ValueError):
